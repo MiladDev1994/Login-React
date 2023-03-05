cfb04893158c197dd7dce8e6ce70bb8991f9f333
MiladDev1994 <miladgolinezhad73@gmail.com>
2023-03-06 00:26:03 +0330

    complet first level register

--- src/Register/Register.js
+++ src/Register/Register.js
@@ -1,68 +1,195 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './register.module.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const Register = () => {
 
-    const [nameFocus , setNameFocus] = useState({focus:false, value: '',});
-    const [emailFocus , setEmailFocus] = useState({focus:false, value: '',});
-    const [passwordFocus , setPasswordFocus] = useState({focus:false, value: '',});
-    const [cpasswordFocus , setCPasswordFocus] = useState({focus:false, value: '',});
+
+    const [data , setData] = useState({
+        name:'',
+        email:'',
+        password:'',
+        confirmPassword:'',
+        law:false,
+    });
+
+    const [focus , setFocus] = useState({
+        name: false,
+        email: false,
+        password: false,
+        confirmPassword: false,
+        law:false,
+    })
+
+    const [active , setActive] = useState({
+        name: false,
+        email: false,
+        password: false,
+        confirmPassword: false,
+        law:false,
+    })
+
+    const [errors , setErrors] = useState({})
+
+    const valueHandeler = (event) => {
+        if (event.target.name === "law"){
+            setData({...data , [event.target.name] : event.target.checked})
+            setActive({...active , [event.target.name] : true})
+        }else {
+        setData({...data , [event.target.name] : event.target.value})
+        }
+    }
+
+    const focusHandeler = (event) => {
+        setFocus({...focus , [event.target.name] : true})
+    }
+
+    const blurHandeler = (event) => {
+        setFocus({...focus , [event.target.name] : false})
+        setActive({...active , [event.target.name] : true})
+    }
 
 
+
+    useEffect(() => {
+
+        const error = {};
+        if (data.name === ''){
+            error.name = "Name is required";
+        }else {
+            delete error.name
+        }
+
+        if (data.email === ''){
+            error.email = "Email is required";
+        }else if (!data.email.includes("@gmail.com")){
+            error.email = "Email is undefined";
+        }else {
+            delete error.email
+        }
+
+        if (data.password === ''){
+            error.password = "Password is required";
+        }else if (data.password.length < 5){
+            error.password = "Password haveTo be more 5 items";
+        }else {
+            delete error.password
+        }
+
+        if (data.confirmPassword === ''){
+            error.confirmPassword = "ConfirmPassword is required";
+        }else if (data.confirmPassword !== data.password){
+            error.confirmPassword = "ConfirmPassword is false";
+        } else {
+            delete error.confirmPassword
+        }
+
+        if (data.law === false){
+            error.law = "Law is required";
+        }else {
+            delete error.law
+        }
+
+        setErrors(error)
+
+    } , [data])
+
+
+    const formHandeler = (e) => {
+        e.preventDefault()
+        const validate = Object.keys(errors)
+        if (validate.length > 1){
+            setActive({
+                name: true,
+                email: true,
+                password: true,
+                confirmPassword: true,
+                law:true,
+            })
+            toast.error('Signing Filed', {
+                position: "bottom-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+            });
+
+        }else {
+            toast.success('Signing successful', {
+                position: "bottom-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+            });
+        }
+    }
+
+    console.log(active)
     return (
         <div className={`container mt-3 text-light ${styles.registerBox}`}>
             <div className={"container-fluid"}>
                 <div className={"row d-flex align-items-center justify-content-center"}>
-                    <div className={`bg-dark rounded-2 shadow d-flex align-items-center justify-content-center flex-column py-3 ${styles.formBox}`}>
+                    <form onSubmit={formHandeler} className={`bg-dark rounded-4 shadow d-flex align-items-center justify-content-center flex-column py-3 ${styles.formBox}`}>
                         <h2>Register</h2>
                         <div className={styles.inputBox}>
                             <div className={styles.nameLabelBox}>
-                                <label className={nameFocus.focus || nameFocus.value !== '' ? styles.nameLabelFocus : styles.nameLabelBlur}>Name</label>
+                                <label className={focus.name || data.name !== '' ? styles.nameLabelFocus : styles.nameLabelBlur}>Name</label>
                             </div>
-                            <input type={"text"} className={`input-group-text w-100 bg-dark ${styles.nameInput}`} value={nameFocus.value} onChange={event => setNameFocus({...nameFocus , value:event.target.value})} onFocus={() => setNameFocus({...nameFocus ,focus:true})} onBlur={() => setNameFocus({...nameFocus , focus:false})}/>
-                            <span>Name is required</span>
+                            <input type={"text"} name={"name"} className={`input-group-text w-100 bg-dark ${styles.nameInput}`} value={data.name} onChange={valueHandeler} onFocus={focusHandeler} onBlur={blurHandeler}/>
+                            {errors.name && active.name ? <span>{errors.name}</span> : ''}
                         </div>
 
                         <div className={styles.inputBox}>
                             <div className={styles.nameLabelBox}>
-                                <label className={emailFocus.focus || emailFocus.value !== '' ? styles.nameLabelFocus : styles.nameLabelBlur}>Email</label>
+                                <label className={focus.email || data.email !== '' ? styles.nameLabelFocus : styles.nameLabelBlur}>Email</label>
                             </div>
-                            <input type={"email"} className={`input-group-text w-100 bg-dark ${styles.nameInput}`} value={emailFocus.value} onChange={event => setEmailFocus({...emailFocus , value:event.target.value})} onFocus={() => setEmailFocus({...emailFocus ,focus:true})} onBlur={() => setEmailFocus({...emailFocus , focus:false})}/>
-                            <span>Email is required</span>
+                            <input type={"text"} name={"email"} className={`input-group-text w-100 bg-dark ${styles.nameInput}`} value={data.email} onChange={valueHandeler} onFocus={focusHandeler} onBlur={blurHandeler}/>
+                            {errors.email && active.email ? <span>{errors.email}</span> : ''}
                         </div>
 
                         <div className={styles.inputBox}>
                             <div className={styles.nameLabelBox}>
-                                <label className={passwordFocus.focus || passwordFocus.value !== '' ? styles.nameLabelFocus : styles.nameLabelBlur}>Password</label>
+                                <label className={focus.password || data.password !== '' ? styles.nameLabelFocus : styles.nameLabelBlur}>Password</label>
                             </div>
-                            <input type={"password"} className={`input-group-text w-100 bg-dark ${styles.nameInput}`} value={passwordFocus.value} onChange={event => setPasswordFocus({...passwordFocus , value:event.target.value})} onFocus={() => setPasswordFocus({...passwordFocus ,focus:true})} onBlur={() => setPasswordFocus({...passwordFocus , focus:false})}/>
-                            <span>Password is required</span>
+                            <input type={"password"} name={"password"} className={`input-group-text w-100 bg-dark ${styles.nameInput}`} value={data.password} onChange={valueHandeler} onFocus={focusHandeler} onBlur={blurHandeler}/>
+                            {errors.password && active.password ? <span>{errors.password}</span> : ''}
                         </div>
 
                         <div className={styles.inputBox}>
                             <div className={styles.nameLabelBox}>
-                                <label className={cpasswordFocus.focus || cpasswordFocus.value !== '' ? styles.nameLabelFocus : styles.nameLabelBlur}>Confirm Password</label>
+                                <label className={focus.confirmPassword || data.confirmPassword !== '' ? styles.nameLabelFocus : styles.nameLabelBlur}>Confirm Password</label>
                             </div>
-                            <input type={"text"} className={`input-group-text w-100 bg-dark ${styles.nameInput}`} value={cpasswordFocus.value} onChange={event => setCPasswordFocus({...cpasswordFocus , value:event.target.value})} onFocus={() => setCPasswordFocus({...cpasswordFocus ,focus:true})} onBlur={() => setCPasswordFocus({...cpasswordFocus , focus:false})}/>
-                            <span>ConfirmPassword is required</span>
+                            <input type={"text"} name={"confirmPassword"} className={`input-group-text w-100 bg-dark ${styles.nameInput}`} value={data.confirmPassword} onChange={valueHandeler} onFocus={focusHandeler} onBlur={blurHandeler}/>
+                            {errors.confirmPassword && active.confirmPassword ? <span>{errors.confirmPassword}</span> : ''}
                         </div>
 
                         <div className={`mt-3 ${styles.checkbox}`}>
-                            <label className={"opacity-50"}>Accept All Law</label>
-                            <input className={"form-check-input"} type={"checkbox"}/>
+                            <div>
+                                <label className={"opacity-50"}>Accept All Law</label>
+                                <input name={"law"} className={"form-check-input"} type={"checkbox"} onChange={valueHandeler}/>
+                            </div>
+                            {active.law &&  errors.law  ? <span className={styles.span}>{errors.law}</span> : ''}
                         </div>
-                        <span className={styles.span}>Law is required</span>
 
                         <div className={styles.inputBox}>
                             <button className={"m-auto d-block btn btn-success mt-5"}>Signin</button>
                         </div>
 
-                    </div>
+                    </form>
                 </div>
             </div>
-
+            <ToastContainer />
         </div>
+
     );
 };
 
